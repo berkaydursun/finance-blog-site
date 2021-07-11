8e92484ecaa30facc1cafb008bd780e69b226821
Berkay Dursun <berkay_dursun@outlook.com>
2021-07-11 19:23:57 +0300

    port fixe

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const blogRoutes = require('./routes/blogRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const indexRoutes = require('./routes/indexRoutes');
 
-
+const PORT = process.env.PORT || 3000;
 
 
 // Connect to MongoDB
 const dbURI = "mongodb+srv://berkay:berkaydursun08@cluster0.onitz.mongodb.net/finance-blog?retryWrites=true&w=majority"
 mongoose.connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then((result) => app.listen(3000))
+    .then((result) => app.listen(PORT))
     .catch((err) => console.log(err));
 
 
